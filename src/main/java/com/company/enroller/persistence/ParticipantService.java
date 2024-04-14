0b0b4a68d76bca389f88bf2f931558f54b6bb35c
boooiler <sebastianowczarek8@gmail.com>
2024-04-14 15:56:37 +0200

    create CRUD operations

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +21,31 @@ public class ParticipantService {
 		String hql = "FROM Participant";
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
+	}
+
+	public Participant findByLogin(String login) {
+		String hql = "FROM Participant WHERE login = :login";
+		Query<Participant> query = connector.getSession().createQuery(hql, Participant.class);
+		query.setParameter("login", login);
+		return query.uniqueResult();
+	}
+
+	public void add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().remove(participant);
+		transaction.commit();
+	}
+
+	public void update(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(participant);
+		transaction.commit();
 	}
 
 }

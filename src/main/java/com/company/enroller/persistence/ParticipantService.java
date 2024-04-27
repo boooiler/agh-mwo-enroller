f416fbdac2591dfa21fff01361a84de39031f838
boooiler <sebastianowczarek8@gmail.com>
2024-04-27 14:11:35 +0200

    Handle query params

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -1,6 +1,7 @@
 package com.company.enroller.persistence;
 
 import java.util.Collection;
+import java.util.Objects;
 
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -17,9 +18,16 @@ public class ParticipantService {
 		connector = DatabaseConnector.getInstance();
 	}
 
-	public Collection<Participant> getAll() {
-		String hql = "FROM Participant";
+	public Collection<Participant> getAll(String sortBy, String sortOrder, String key) {
+		String hql = "FROM Participant WHERE login LIKE :loginValue";
+		if(sortBy.equals("login") || sortBy.equals("password")) {
+			hql += " ORDER BY " + sortBy;
+			if(sortOrder.equals("ASC") || sortOrder.equals("DESC")){
+				hql += " " + sortOrder;
+			}
+		}
 		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("loginValue", "%" + key + "%");
 		return query.list();
 	}
 
